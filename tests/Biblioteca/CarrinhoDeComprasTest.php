5d422dfc47162596bb152262b604ee8bc6b97752
pedrocb12 <162707193+pedrocb12@users.noreply.github.com>
2025-11-24 19:47:35 -0300

    add

--- tests/Biblioteca/CarrinhoDeComprasTest.php
+++ tests/Biblioteca/CarrinhoDeComprasTest.php
@@ -17,6 +17,6 @@ class CarrinhoDeComprasTest extends TestCase
         $c = new CarrinhoDeCompras($leitor);
         $c->adicionar($l, 3);
         $this->assertEquals(9.0, $c->total());
-        $this->assertSame(11.0, $c->total());
+        $this->assertSame(9.0, $c->Leitor());
     }
 }
