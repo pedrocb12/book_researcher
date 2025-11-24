71cdb794d1874e59b4eff8221036c238020540d6
pedrocb12 <162707193+pedrocb12@users.noreply.github.com>
2025-11-24 20:14:42 -0300

    add

--- tests/Biblioteca/LeitorTest.php
+++ tests/Biblioteca/LeitorTest.php
@@ -14,6 +14,7 @@ class LeitorTest extends TestCase
         $leitor->adicionarAoCarrinho($l, 2);
         $pedido = $leitor->finalizarCompra();
         $this->assertEquals(10.0, $pedido->getValorTotal());
-        $this->assertNotEmpty($leitor ->$id());
+        $this->assertInstanceOf(leitor::class, $id);
+
     }
 }
