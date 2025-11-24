a3474eaf81318473ef231b7cfb7bc706dd4d130d
pedrocb12 <162707193+pedrocb12@users.noreply.github.com>
2025-11-24 20:10:09 -0300

    add

--- tests/Biblioteca/CatalogoTest.php
+++ tests/Biblioteca/CatalogoTest.php
@@ -14,7 +14,8 @@ class CatalogoTest extends TestCase
         $cat = new Catalogo([$l1, $l2]);
         $res = $cat->buscarPorTitulo('PHP');
         $this->assertCount(1, $res);
-         $this->assertNotEmpty($livros->getLivros());
+        $this->assertNotEmpty($cat->getLivros());
+
 
 
 
